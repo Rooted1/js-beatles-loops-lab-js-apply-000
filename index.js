7ff5b79f397a10561514c3919719bb17589c3b8a
Ruth Obe <ruthobey@gmail.com>
2019-10-07 21:40:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+function theBeatlesPlay(arr1, arr2){
+  var emptyArr = []
+  
+  for (var i in arr1){
+    for(var j in arr2){
+      return arr1[i] + "plays " + arr2[j]
+    }
+  }
+  
+}
